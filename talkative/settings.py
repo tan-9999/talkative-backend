8cb3485d30281d721854c6f993dad894cc2d2ec3
tan-9999 <tanmays9999@gmail.com>
2026-03-17 16:58:52 +0530

    update db setting

--- talkative/settings.py
+++ talkative/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Read DATABASE_URL from env (Supabase connection string)
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
+        conn_max_age=200,
         ssl_require=True,  # Supabase enforces SSL
     )
 }
